c16b26f39803d7f5924e7d68910e5475454e4a33
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-04-01 21:48:56 +0200

    Remove logging at startup

--- mopidy_orfradio/backend.py
+++ mopidy_orfradio/backend.py
@@ -19,9 +19,3 @@ class ORFBackend(pykka.ThreadingActor, backend.Backend):
         self.library = ORFLibraryProvider(backend=self)
         self.playback = ORFPlaybackProvider(audio=audio, backend=self)
         self.uri_schemes = ["orfradio"]
-
-    def on_start(self):
-        logger.info("Starting ORFBackend")
-
-    def on_stop(self):
-        pass
